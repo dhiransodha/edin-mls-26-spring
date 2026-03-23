58e016ccc1507bee1d131af8011879c1a12cefb4
maxbraith <maxbraithwaite@gmail.com>
2026-03-23 12:30:39 +0000

    num_warps=8 & num_stages=4

--- hw1-asr/glm_asr_triton_template/layers.py
+++ hw1-asr/glm_asr_triton_template/layers.py
@@ -821,6 +821,8 @@ class Linear:
             BLOCK_M=self.TILE_M,
             BLOCK_N=self.TILE_N,
             BLOCK_K=self.TILE_K,
+            num_warps=8,
+            num_stages=4,
         )
 
         output = output[:M, :N]
